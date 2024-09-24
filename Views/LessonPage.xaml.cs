938e306aa282ae4f3704207c716014a51f9ccebb
unknown <dawidgrabolus@gmail.com>
2024-09-24 17:42:16 +0200

    random month, check answer

---
+++ Views/LessonPage.xaml.cs
@@ -0,0 +1,53 @@
+using Monthy.Models;
+
+namespace Monthy.Views;
+
+public partial class LessonPage : ContentPage
+{
+    private readonly List<Month> months = new List<Month>
+        {
+            new Month(1, "styczeñ"),
+            new Month(2, "luty"),
+            new Month(3, "marzec"),
+            new Month(4, "kwiecieñ"),
+            new Month(5, "maj"),
+            new Month(6, "czerwiec"),
+            new Month(7, "lipiec"),
+            new Month(8, "sierpieñ"),
+            new Month(9, "wrzesieñ"),
+            new Month(10, "paŸdziernik"),
+            new Month(11, "listopad"),
+            new Month(12, "grudzieñ")
+        };
+    private Random random = new Random();
+    private int currentMonthNumber; 
+
+    public LessonPage()
+    {
+        InitializeComponent();
+
+        currentMonthNumber = random.Next(1, 12);
+        monthLabel.Text = Convert.ToString(months[currentMonthNumber].Number);
+
+        Button send = this.FindByName<Button>("send");
+        send.Clicked += OnSendButtonClicked;
+    }
+
+    private void OnSendButtonClicked(object sender, EventArgs e)
+    {
+        string userAnswer = entry.Text?.Trim().ToLower();
+        Month currentMonth = months.Find(m => m.Number == currentMonthNumber+1);
+
+        if (currentMonth != null)
+        {
+            if (userAnswer == currentMonth.Name.ToLower())
+            {
+                DisplayAlert("Result", "Correct!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Result", "Incorrect! Try again.", "OK");
+            }
+        }
+    }
+}
